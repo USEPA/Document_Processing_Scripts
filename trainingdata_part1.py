852482ab27b461275fa95cd974eb0fbaea31b028
Andrew Yuen <yuen.andrew@epa.gov>
2019-12-06 20:26:50 -0500

    Update comments

--- trainingdata_part1.py
+++ trainingdata_part1.py
@@ -21,7 +21,7 @@ for filename in qq:
     # Get the filename
     file = filename.split('\\')[-1]
     fileext = filename.split(".")[-1]
-    # For ezEmail, if file is in the pdf folder extract record ID from end of filename
+    # For ezEmail - email content, if file is in the pdf folder extract record ID from end of filename
     if 'pdf' in filename and 'attachment' not in filename and fileext.lower() not in filterout:
         print(file.rsplit( ".", 1)[0].rsplit('_', 1)[1])
         recordid = file.rsplit( ".", 1)[0].rsplit('_', 1)[1]
@@ -30,7 +30,7 @@ for filename in qq:
         if recordid in filename:
             # copyfile(filename, copypath+recordid+'\\'+file)
             shutil.move(filename, copypath+recordid+'\\'+file)  
-    # For ezEmail, if file is in the pdf folder extract record ID from begining of filename
+    # For ezEmail - attachment, if file is in the attachment folder extract record ID from begining of filename
         continue
     if 'attachment' in filename and fileext.lower() not in filterout:
         print(file.rsplit( ".", 1)[0].rsplit('_')[0])
